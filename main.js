f89b45aa89915d953f52bf9597e3564101abc56d
willis-morse <willis.morse@thermofisher.com>
2024-02-04 14:04:58 -0500

    intial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const morgan = require("morgan");
+
+
+const app = express()
+const port = 3000
+
+// set up morgan middleware
+app.use(morgan("dev"));
+
+app.get('/', (req, res) => {
+  res.send('')
+})
+
+app.post('/', (req, res) => {
+    res.send('{}')
+  })
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
